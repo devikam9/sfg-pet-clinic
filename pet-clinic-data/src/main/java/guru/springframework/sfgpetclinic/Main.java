0a35392b5bd3204913afa827209863cc39edd081
Devika <devika@Devikas-MacBook-Air.local>
2024-10-01 13:08:03 -0700

    interfaces for services

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Main.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Main.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic;
 
+import guru.springframework.sfgpetclinic.model.Person;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,5 +15,8 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+    }
+
+    public static class Vet extends Person {
     }
 }
