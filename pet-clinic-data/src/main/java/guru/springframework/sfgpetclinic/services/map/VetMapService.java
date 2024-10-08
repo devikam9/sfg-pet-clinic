b2e8793fa79dbfa47b6c1e063cdf44dd0c7ec292
Devika <devika@Devikas-MacBook-Air.local>
2024-10-08 15:52:19 -0700

    Added jpa profiles and tested profiles

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
@@ -6,10 +6,12 @@ import guru.springframework.sfgpetclinic.model.Vet;
 
 import guru.springframework.sfgpetclinic.services.SpecialitiesService;
 import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 @Service
+@Profile({"default","map"})
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
     private SpecialitiesService specialitiesService;
